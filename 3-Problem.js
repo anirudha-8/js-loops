da112567d167bcf81eab6343ed2c80fcc47b8dac
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-28 16:43:33 +0530

    feat(3-Problem.js): add solution for 3-Problem

---
+++ 3-Problem.js
@@ -0,0 +1,7 @@
+// 3-Problem statement - Create a for...of loop to log each character of the string "JavaScript".
+
+const givenString = "JavaScript";
+
+for (const char of givenString) {
+	console.log(char);
+}
