568adae75d04284cbea6c76df5b8dbed1d1337f8
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-28 16:58:17 +0530

    feat(6-Problem.js): add solution for 6-Problem

---
+++ 6-Problem.js
@@ -0,0 +1,9 @@
+// 6-Problem statement - Create a for loop that calculates the factorial of 5.
+
+let fact = 1;
+
+for (let i = 2; i <= 5; i++) {
+	fact *= i;
+}
+
+console.log(`Factorial of 5: ${fact}`);
