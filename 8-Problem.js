e9a629231f532746be154c1031670426de7c842b
Anirudha Bele <beleanirudha8@gmail.com>
2025-03-01 11:16:45 +0530

    perf(8-Problem.js): add in-place array reversal method to 8-Problem.js for improved efficiency
    - optimize efficiency, no extra space required
    - solve using 2-pointer approach

--- 8-Problem.js
+++ 8-Problem.js
@@ -2,11 +2,20 @@
 
 const numbersArray = [1, 2, 3, 4];
 
+// Use when don't need to mutate / modify the original array
 let reversedNumbersArray = [];
-
 for (let i = numbersArray.length - 1; i >= 0; i--) {
 	reversedNumbersArray.push(numbersArray[i]);
 }
-
 // console.log(`Reversed array is: ${reversedNumbersArray}`);
 console.log("Reversed array is: ", reversedNumbersArray);
+
+// Use when in-place replacement is required
+// optimized solution, no extra space required
+console.log("original array is: ", numbersArray);
+for (let i = 0; i < numbersArray.length / 2; i++) {
+	let temp = numbersArray[i];
+	numbersArray[i] = numbersArray[numbersArray.length - 1 - i];
+	numbersArray[numbersArray.length - 1 - i] = temp;
+}
+console.log("after revering array is: ", numbersArray);
